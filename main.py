21665fe7ddf9d76652a02c5ddb98d911cc899667
peanut1614 <ratnalinrp@gmail.com>
2023-11-29 11:03:07 +0700

    calculator

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter as tkinter
+
+class mycalculator:
+    def_init_(self):
+
+        self.root = tk.Tk()
+
+        self.root.geometry("300x300")
+        self.root.title('my calculator')
+
+        self.label = tk.Label(self.root, text='Hello DIP01', font=('Arial', 18))
+        self.label.pack()
+
+        self.button=tk.Button(self.root, text='Click here', height=4)
+        self.button.place(x=20, y=50)
+
+        self.root.mainloop()
+
+mycalculator()        
