771cb35cd5e72b1b8803c7f3da7a086f22eaf470
peanut1614 <ratnalinrp@gmail.com>
2023-12-06 10:28:18 +0700

    cal ที่ action ได้

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import tkinter as tk
 
 class mycalculator:
+    title = "my Calculator"
     def __init__(self):
 
         self.root = tk.Tk()
+        self.label_taxt = tk.StringVar()
+        self.label_taxt.set("Hello DIP01")
 
         self.root.geometry("300x300")
-        self.root.title('my calculator')
+        self.root.title(self.title)
 
-        self.label = tk.Label(self.root, text='Hello DIP01', font=('Arial', 18))
+        self.label = tk.Label(self.root, textvariable=self.label_taxt, font=('Arial', 18))
         self.label.pack()
+
+        self.button_trig = tk.Button(self.root, text="trigger change title to \"Test\"", height=4, )
+        self.button_my_cal = tk.Button(self.root, text="trigger change title to \"My Calculator\"", height=4, )
+        
 
         self.button=tk.Button(self.root, text='C', height=3, width=7)
         self.button.place(x=20, y=50)
@@ -25,6 +32,8 @@ class mycalculator:
 
         self.button=tk.Button(self.root, text='0', height=3, width=16)
         self.button.place(x=20, y=290)
+
+        self.button.bind( "<Button-1>" , self.action0)
 
         self.button=tk.Button(self.root, text='+/-', height=3, width=7)
         self.button.place(x=85, y=50)
@@ -69,5 +78,13 @@ class mycalculator:
         self.button.place(x=215, y=290)
 
         self.root.mainloop()
+
+    def action0(self, event):
+        print(event)
+        self.title = "Test"
+        self.root.title(self.title)
+        self.label_taxt.set("DIP 01")
+        self.label.pack()
+    
 
 mycalculator()        
