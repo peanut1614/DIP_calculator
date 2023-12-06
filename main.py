d01b8f070a36007f8c03c46f8c71cde6990531ac
Fryingpanwithegg <hw45512@horwang.ac.th>
2023-12-06 11:36:02 +0700

    fararay

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class mycalculator:
 
         self.root = tk.Tk()
         self.label_taxt = tk.StringVar()
-        self.label_taxt.set("Hello DIP01")
+        self.label_taxt.set("")
 
         self.root.geometry("300x300")
         self.root.title(self.title)
@@ -30,6 +30,8 @@ class mycalculator:
         self.button=tk.Button(self.root, text='1', height=3, width=7)
         self.button.place(x=20, y=230)
 
+        self.button.bind( "<Button-1>" , self.action1)
+
         self.button=tk.Button(self.root, text='0', height=3, width=16)
         self.button.place(x=20, y=290)
 
@@ -41,8 +43,12 @@ class mycalculator:
         self.button=tk.Button(self.root, text='8', height=3, width=7)
         self.button.place(x=85, y=110)
 
+        self.button.bind( "<Button-1>" , self.action8)
+
         self.button=tk.Button(self.root, text='5', height=3, width=7)
         self.button.place(x=85, y=170)
+
+        self.button.bind( "<Button-1>" , self.action5)
 
         self.button=tk.Button(self.root, text='2', height=3, width=7)
         self.button.place(x=85, y=230)
@@ -83,8 +89,29 @@ class mycalculator:
         print(event)
         self.title = "Test"
         self.root.title(self.title)
-        self.label_taxt.set("DIP 01")
+        self.label_taxt.set(self.label_taxt.get()+"0")
         self.label.pack()
+
+    def action1(self, event):
+        print(event)
+        self.title = "Test"
+        self.root.title(self.title)
+        self.label_taxt.set(self.label_taxt.get()+"1")
+        self.label.pack()
+
+    def action5(self, event):
+        print(event)
+        self.title = "Test"
+        self.root.title(self.title)
+        self.label_taxt.set(self.label_taxt.get()+"5")
+        self.label.pack()
+
+    def action8(self, event):
+        print(event)
+        self.title = "Test"
+        self.root.title(self.title)
+        self.label_taxt.set(self.label_taxt.get()+"8")
+        self.label.pack()        
     
 
 mycalculator()        
