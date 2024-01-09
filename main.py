e9a2b9290e0cf7b7bf9e6a2bab5e8f1a1e22fa85
Fryingpanwithegg <hw45512@horwang.ac.th>
2024-01-09 23:11:14 +0700

    การแสดงค่าที่กด+สร้างตัวแปรเครื่องหมาย

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
- def bind_keys(self):
+def bind_keys(self):
         self.window.bind("<Return>", lambda event: self.evaluate())
         for key in self.digits:
             self.window.bind(str(key), lambda event, digit=key: self.add_to_expression(digit))
@@ -6,7 +6,7 @@
         for key in self.operations:
             self.window.bind(key, lambda event, operator=key: self.append_operator(operator))
 
-    def create_special_buttons(self):
+def create_special_buttons(self):
         self.create_clear_button()
         self.create_equals_button()
         self.create_square_button()
