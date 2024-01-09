ad051e79e369e56877c9488abd9c0e9a848024c0
peanut1614 <ratnalinrp@gmail.com>
2024-01-09 17:12:12 +0700

    update calculator

--- main.py
+++ main.py
@@ -103,6 +103,26 @@ class mycalculator:
         self.root.title(self.title)
         self.label_taxt.set(self.label_taxt.get()+"/")
         self.label.pack()
+
+     def create_display_labels(self):
+        total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                               fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
+        total_label.pack(expand=True, fill='both')
+
+        label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                         fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)
+        label.pack(expand=True, fill='both')
+
+        return total_label, label
+
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def add_to_expression(self, value):
+        self.current_expression += str(value)
+        self.update_label()
     
 
 mycalculator()        
